e7970ec2f3d46c417206512cb38f765dc198f9b0
Emmannuel Sunday <sunasunday111@gmail.com>
2020-07-21 08:30:32 +0300

    3rd commit (any name)

---
+++ application/controllers/hospital/Dashboard.js
@@ -0,0 +1,12 @@
+exports.dashboardView = (req, res, next) => {
+	randerForDashBoard(req, res, {
+		title: "Dasboard",
+		layout: "dashboard",
+	})
+};
+
+exports.profileView = (req, res, next) => {
+};
+
+exports.profile = (req, res, next) => {
+};
